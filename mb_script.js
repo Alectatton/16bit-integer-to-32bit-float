e332fa7826ae1abcb2e70bd4632e6fd913188136
Alec Tatton <alectatton1@gmail.com>
2020-12-13 15:03:23 -0700

    12/13

--- mb_script.js
+++ mb_script.js
@@ -21,6 +21,18 @@ function calculation() {
     //converting 4 byte arrayBuffer into a float and saving to variable val
     val = view.getFloat32(0, false);
 
+    var node1 = document.createElement("li");
+    node1.appendChild(document.createTextNode(num1));
+    document.querySelector('#list1').appendChild(node1);
+
+    var node2 = document.createElement("li");
+    node2.appendChild(document.createTextNode(num2));
+    document.querySelector('#list2').appendChild(node2);
+
+    var node3 = document.createElement("li");
+    node3.appendChild(document.createTextNode(val));
+    document.querySelector('#list3').appendChild(node3);
+
     //output to HTML
-    document.querySelector("#value").innerHTML = val;
+    //document.querySelector("#value").innerHTML = val;
 };
